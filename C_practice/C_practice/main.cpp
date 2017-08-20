41b1a1b56ec139bb959aa3f0d2c170a632e19ac2
linussp <linus5211@gmail.com>
2017-08-20 09:12:14 -0500

    initialize project

---
+++ C_practice/C_practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C_practice
+//
+//  Created by 周劭衡 on 20/08/2017.
+//  Copyright © 2017 周劭衡. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
